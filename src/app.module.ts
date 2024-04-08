7793ad71517832ab89782692fc9265fa6e378972
Tiago <tiagoufcg@gmail.com>
2024-04-08 19:37:58 -0300

    aula protegendo rotas com Guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,19 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
+import { AuthenticateController } from './controllers/authenticate.controller'
+import { CreateQuestionController } from './controllers/create-question.controller'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
     validate: (env) => envSchema.parse(env),
     isGlobal: true,
-  })],
-  controllers: [CreateAccountController],
-  providers: [PrismaService,],
+  }),
+  AuthModule,
+],
+  controllers: [CreateAccountController, AuthenticateController, CreateQuestionController],
+  providers: [PrismaService],
 })
 export class AppModule {}
